fdfea59809669516e2957fda19a1a1dd7e0d1faf
Fabio <fabio.biscaro@gmail.com>
2016-01-15 22:12:42 +0100

    primo commit

---
+++ app/src/main/java/meomobile/it/sunshine/MainActivityFragment.java
@@ -0,0 +1,133 @@
+package meomobile.it.sunshine;
+
+import android.os.AsyncTask;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayList<String> weekForecast = new ArrayList<String>();
+
+        weekForecast.add("Anno");
+        weekForecast.add("Gatto");
+        weekForecast.add("Come");
+        weekForecast.add("Va");
+        weekForecast.add("Ciao");
+        weekForecast.add("Gatto");
+        weekForecast.add("Come");
+        weekForecast.add("Va");
+        weekForecast.add("Ciao");
+        weekForecast.add("Gatto");
+        weekForecast.add("Come");
+        weekForecast.add("Va");
+        weekForecast.add("Ciao");
+        weekForecast.add("Gatto");
+        weekForecast.add("Come");
+        weekForecast.add("Va");
+
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
+        ListView lv = (ListView) rootView.findViewById(R.id.listview_forecast);
+
+        lv.setAdapter(aa);
+
+        return rootView;
+    }
+
+
+    public class FetchWeatherTask extends AsyncTask {
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            // Weather!
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+
+            Log.d("PlaceholderFragment", forecastJsonStr);
+
+            return forecastJsonStr;
+        }
+    }
+
+}
