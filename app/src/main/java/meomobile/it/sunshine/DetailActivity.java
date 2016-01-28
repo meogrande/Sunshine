90a4ecb80ab0bdc3a1183af6b01540193ac352da
Fabio <fabio.biscaro@gmail.com>
2016-01-28 21:32:45 +0100

    Ora si legge quello che si è cliccato! :)

--- app/src/main/java/meomobile/it/sunshine/DetailActivity.java
+++ app/src/main/java/meomobile/it/sunshine/DetailActivity.java
@@ -16,16 +16,20 @@
 
 package meomobile.it.sunshine;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class DetailActivity extends ActionBarActivity {
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +78,16 @@ public class DetailActivity extends ActionBarActivity {
                                  Bundle savedInstanceState) {
 
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            // Prende i dati dall'intento
+            Intent i = getActivity().getIntent();
+            String s = i.getStringExtra(Intent.EXTRA_TEXT);
+            Log.d(this.getClass().toString(), s);
+
+            // metto la stringa nella label
+            TextView tv = (TextView) rootView.findViewById(R.id.text_hello_world);
+            tv.setText(s);
+
             return rootView;
         }
     }
